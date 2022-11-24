46625f698c3f5d470f2b4058aaed3c2c096fc042
Amiel Christian Mala-ay <88815448+amielchristian@users.noreply.github.com>
2022-11-24 13:03:00 +0800

    Move Login and Logout servlets to controller

--- src/java/controller/Login.java
+++ src/java/controller/Login.java
@@ -1,10 +1,11 @@
-package model;
+package controller;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 
+import model.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
