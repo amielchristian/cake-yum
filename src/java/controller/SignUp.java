c9b992d7837fed94dd999647612ffd188ad6ccae
Patricia Denise Poblete <patriciadenise.poblete.cics@ust.edu.ph>
2023-05-10 22:00:11 +0800

    ADDED: Added Security in Sign up

--- src/java/controller/SignUp.java
+++ src/java/controller/SignUp.java
@@ -52,12 +52,13 @@ public class SignUp extends HttpServlet {
                 String fullName = request.getParameter("full-name");
                 String address = request.getParameter("address");
                 String celNum = request.getParameter("mobile-number");
-
+                Security sec = new Security(getServletContext().getInitParameter("key"), getServletContext().getInitParameter("cipherTransformation"));
+               
                 try {
                     String query = "INSERT INTO USERS(USER_UNAME, USER_PASSWORD, USER_GIVEN_NAME, USER_ADDRESS, USER_CONTACT_NUM) VALUES (?,?,?,?,?)";
                     PreparedStatement ps = conn.prepareStatement(query);
                     ps.setString(1, username);
-                    ps.setString(2, Security.encrypt(password));
+                    ps.setString(2, sec.encrypt(password));
                     ps.setString(3, fullName);
                     ps.setString(4, address);
                     ps.setString(5, celNum);
